3684f24bd8f0217e92ba3f8b8a627a7ad96b2872
grandeljay <github@grandels.email>
2024-09-26 12:26:47 +0200

    chore(module): update to v0.11.0

--- src-mmlc/Constants.php
+++ src-mmlc/Constants.php
@@ -4,7 +4,7 @@ namespace Grandeljay\CaoProductVariants;
 
 class Constants
 {
-    private const MODULE_VERSION = '0.10.1';
+    private const MODULE_VERSION = '0.11.0';
 
     public const MODULE_PRODUCT_NAME    = 'MODULE_PRODUCT_GRANDELJAY_CAO_PRODUCT_VARIANTS_PRODUCT';
     public const MODULE_PRODUCT_VERSION = self::MODULE_VERSION;
